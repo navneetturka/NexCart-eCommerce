3e0a15f89d8502229af2b65665c1185db515e23c
HimanshiJolly <himanshijolly3@gmail.com>
2026-03-23 11:09:31 +0530

    added search bar functionality in navdar added filtering of products based on search query

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
 import { assets } from '../assets/assets'
 import { NavLink, Link } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
 
     const [visible, setVisible] = useState(false)
-
+    const{setShowSearch}=useContext(ShopContext)
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             
@@ -37,7 +38,7 @@ const Navbar = () => {
 
             <div className='flex items-center gap-6'>
 
-                <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+                <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
                 <div className='relative group'>
                     <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {use, useContext, useEffect, useState} from 'react'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import Title from '../components/Title';
@@ -6,13 +6,14 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
   // To get data of all products using context api
-  const {products}=useContext(ShopContext);
 
   // State variables
+  const {products, search, showSearch} = useContext(ShopContext);
   const [showFilter,setShowFilter]=useState(false);
   const [filterProducts,setFilterProducts]=useState([]);
   const [category,setCategory]=useState([]);
   const [subCategory,setSubCategory]=useState([]);
+  const [sortType,setSortType]=useState('relavent')
 
   //toggle functions, saving data in category and subcategory
   const toggleCategory = (e) =>{
@@ -34,6 +35,10 @@ const Collection = () => {
   //filter is applied with function
   const applyFilter = () =>{
     let productsCopy = products.slice();
+    if(showSearch && search)
+    {
+      productsCopy=productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()))
+    }
 
     if (category.length>0) {
       productsCopy =productsCopy.filter(item => category.includes(item.category))
@@ -43,10 +48,26 @@ const Collection = () => {
     }
     setFilterProducts(productsCopy);
   }
-  
+  const sortProduct=()=>{
+    let fpcopy=filterProducts.slice();
+    switch(sortType){
+      case 'low-high':
+        setFilterProducts(fpcopy.sort((a,b)=>(a.price-b.price)))
+        break;
+        case'high-low':
+          setFilterProducts(fpcopy.sort((a,b)=>(b.price-a.price)))
+          break;
+          default:
+            applyFilter();
+            break;
+    }
+  }
   useEffect(()=>{
     applyFilter();
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
 
   //the category which we check is store in the category array
   // useEffect(()=>{
@@ -101,7 +122,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'}/>
           {/* Product Sort */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
