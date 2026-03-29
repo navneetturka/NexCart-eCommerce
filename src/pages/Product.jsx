88f9f943109e9f3b50ef2aba04371e0718dd38ca
komalpreetkaur18278@gmail.com <komalpreetkaur18278@gmail.com>
2026-03-29 11:43:30 +0530

    products page completed

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,89 +1,134 @@
-import React, { useEffect, useState, useContext} from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, products } from '../assets/assets'
+import { assets } from '../assets/assets'
 import { ShopContext } from '../context/ShopContext'
-const Product = () => {
-  const { productId } = useParams();
-  const { products,currency } = useContext(ShopContext);
-  const [productData, setProductData] = useState(false);
+import RelatedProducts from '../components/RelatedProducts'
 
-  const[image,setImage]=useState('');
-  const [size,setSize]=useState("");
+const Product = () => {
+
+  const { productId } = useParams();
+  const { products, currency ,addToCart} = useContext(ShopContext);
+
+  const [productData, setProductData] = useState(null);
+  const [image, setImage] = useState('');
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
-    products.map((item)=>{
-      if(item._id===productId){
-        setProductData(item);
-        setImage(item.image[0]);
-        console.log(item)
-        return null;
-      }
-    })
+    if(item._id===productId){
+      setProductData(item)
+      setImage(item.image[0]);
+      return null;
+    }
   }
-  useEffect(()=>{
-    fetchProductData();
-  },[productId,products]);
-  return productData? (
+
+  useEffect(() => {
+    if (products.length > 0) {
+      fetchProductData();
+    }
+  }, [productId, products]);
+
+  return productData ? (
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
-      {/* {product data} */}
+
+      {/* -------- Product Data -------- */}
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
-      {/* {prodct images} */}
-      <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
-        <div className='flex sm:flex-col overflow-x auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
-          {
-            productData.image.map((item,index)=>(
-              <img onClick={()=>setImage(item)} src={item}key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'></img>
-            ))
-          }
-        </div>
-        <div className='w-full sm:w-[80%]'>
-          <img className='w-full h-auto' src={image} alt="" />
-        </div>
-      </div>
-      {/* ----product info----- */}
-      <div className='flex-1'>
-        <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
-        <div className='flex items-center gap-1 mt-2'>
-          <img src={assets.star_icon} alt="" className="w-3 5" />
-          <img src={assets.star_icon} alt="" className="w-3 5" />
-          <img src={assets.star_icon} alt="" className="w-3 5" />
-          <img src={assets.star_icon} alt="" className="w-3 5" />
-          <img src={assets.star_icon_dull_icon} alt="" className="w-3 5" />
-          <p className='pl-2'>(122)</p>
-        </div>
-        <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
-        <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
-        <div className='flex flex-col gap-4 my-8'>
-          <p>Select size</p>
-          <div className='flex gap-2'>
-            {productData.sizes.map((item,index)=>(
-              <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item===size? 'border-orange-500':''}`}  key={index}>{item}</button>
-            ))}
+
+        {/* -------- Product Images -------- */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img
+                  onClick={() => setImage(item)}
+                  src={item}
+                  key={index}
+                  className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'
+                  alt=""
+                />
+              ))
+            }
+          </div>
+
+          <div className='w-full sm:w-[80%]'>
+            <img className='w-full h-auto' src={image} alt="" />
           </div>
         </div>
-        <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
-        <hr className='mt-8 sm:w-4/5'></hr>
-        <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
-          <p>100 % Original produxt.</p>
-          <p>Cash on delivery is available on this product.</p>
-          <p>Easy return and exchange poloucy within 7 days.</p>
+
+        {/* -------- Product Info -------- */}
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+
+          {/* Rating */}
+          <div className='flex items-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon_dull_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
+          </div>
+
+          {/* Price */}
+          <p className='mt-5 text-3xl font-medium'>
+            {currency}{productData.price}
+          </p>
+
+          {/*  Description */}
+          <p className='mt-5 text-gray-500 md:w-4/5'>
+            {productData.description}
+          </p>
+
+          {/* Sizes */}
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select size</p>
+            <div className='flex gap-2'>
+              {productData.sizes.map((item, index) => (
+                  <button
+                    onClick={() => setSize(item)}
+                    className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} />
+                ))}
+            </div>
+          </div>
+
+          {/* Add to Cart */}
+          <button onClick={()=>addToCart(productData._id,size )} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 disabled:bg-gray-400'>ADD TO CART</button>
+
+          <hr className='mt-8 sm:w-4/5' />
+
+          {/*  Info */}
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
-      </div>
-      {/* ---------------description------------ */}
+
+      {/* -------- Description Section -------- */}
       <div className='mt-20'>
         <div className='flex'>
           <b className='border px-5 py-3 text-sm'>Description</b>
-          <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
         </div>
-        <div className='flex flex-col gap-4 border px-6 py-6 text-gray-500'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, eius perferendis. Obcaecati, dolorum? Animi vel officiis molestiae quam nobis, blanditiis ad facere distinctio, praesentium temporibus officia amet quos, non repellendus?</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ex itaque non vitae sed quis, necessitatibus aut nostrum aperiam fugiat quaerat ad expedita alias? Quas exercitationem animi laudantium quis praesentium.</p>
-          
-        </div>
-      </div>
-      </div>
-  ):<div className='opacity-0'></div>
-}
 
-export default Product
+        <div className='flex flex-col gap-4 border px-6 py-6 text-gray-500'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus,
+            eius perferendis. Obcaecati, dolorum?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ex
+            itaque non vitae sed quis.
+          </p>
+        </div>
+      </div>
+
+      {/* -------- Related Products -------- */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
+
+    </div>
+  ) : <div className='opacity-0'></div>
+};
+
+export default Product;
