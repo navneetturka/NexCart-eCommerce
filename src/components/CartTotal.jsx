cc415c63adc1dc579cb3bcb0d5200f0e00e0eac4
HimanshiJolly <himanshijolly3@gmail.com>
2026-04-01 11:12:02 +0530

    added cartpage functionality

---
+++ src/components/CartTotal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const CartTotal = () => {
+    const {currency,delivery_fee,getCartAmount}=useContext(ShopContext);
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+export default CartTotal
