36a89f2b08c9388ce0c5cfbb19932a2df9ba20b9
komalpreetkaur18278@gmail.com <komalpreetkaur18278@gmail.com>
2026-04-20 09:51:51 +0530

    folder name model has to be updated in usercontroller.js file

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel.js";
+import userModel from "../model/userModel.js";
 import validator from "validator";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
